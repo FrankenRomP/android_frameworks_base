a1cb6072af84539422fcc25166d1f5cf43f52e4e
ezio84 <brabus84@gmail.com>
2018-09-04 22:38:53 +0530

    Screenrecord service improvements and QSTile [1/2]
    single tap to toggle quality:
    - 480*800 resolution and 1.5Mbps
    - default resolution (720p) and 4Mbps
    - default resolution (720p) and 8Mbps
    long press to start recording
    
    Show also a blinking red button at bottom right of the screen
    that can be tapped to stop the recording instead of using
    the notification.
    
    Also improve the screenrecord service and be sure
    to stop it when we are done.
    
    part 2/2 is in framework/av tree, it just sets max recording
    duration to 30 minutes instead of 3
    
    @SKULSHADY: Change High quality resolution to 1080p.

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -7634,4 +7634,9 @@ public class WindowManagerService extends IWindowManager.Stub
     public boolean isGestureButtonRegion(int i, int i2) {
         return this.mPolicy.isGestureButtonRegion(i, i2);
     }
+
+    @Override
+    public void screenRecordAction(int mode) {
+        mPolicy.screenRecordAction(mode);
+    }
 }
